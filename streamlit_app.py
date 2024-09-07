e8aea212bf1e4dd27590836f0bb8777cea74ec28
veera161 <gveerachamy@onemagnify.com>
2024-09-07 22:23:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,6 @@ if ingredients_list :
         for furit_chosen in ingredients_list:   
             ingredients_string = furit_chosen + ' '
             search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-            # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
             st.subheader(furit_chosen + 'Nutrition Information')
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
             fv_dt = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
