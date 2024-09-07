d0feefbd5fbf1661c9f478c09c55b3cc58084df2
veera161 <gveerachamy@onemagnify.com>
2024-09-07 18:11:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
+streamlit.title("My parents new health dinner")
 # Write directly to the app
 st.title("Example Streamlit App :balloon:")
 st.write(
